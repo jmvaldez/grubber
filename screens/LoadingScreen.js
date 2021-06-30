1d104f345450d1ca605cc51a75bbdd3b833183a7
Joshua Rutkowski <54906829+joshrutkowski@users.noreply.github.com>
2021-06-30 10:19:31 -0700

    Implemented loading screen with ActivityIndicator

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,25 +1,24 @@
 import React, { Component } from "react";
-import { 
-    View,
-    Text,
-    StyleSheet
-} from "react-native";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 
+/**
+ * Shown to the user when checking if user is logged in to Firebase or not
+ */
 class LoadingScreen extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>LoadingScreen</Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 }
 export default LoadingScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
