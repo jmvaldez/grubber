02ec28c34a1f46d8a01090575a33580dbc4a5d2f
CleverlyDone <pierregober@gmail.com>
2021-07-01 12:58:32 -0700

    dietary labels

--- components/Profile/ProfileScreen0.js
+++ components/Profile/ProfileScreen0.js
@@ -2,8 +2,23 @@ import React from "react";
 import { Button, Image, StatusBar, Text, View } from "react-native";
 import imageStyler from "../../assets/css/image.js";
 import formatStyler from "../../assets/css/format.js";
+import { useEffect } from "react";
 
 function ProfileScreen0({ navigation }) {
+  //This hook for testing onl
+  useEffect(() => {
+    currentUser = firebase.auth().currentUser;
+    console.log(currentUser);
+
+    // fetch("https://jsonplaceholder.typicode.com/photos")
+    //   .then((response) => response.json())
+    //   .then((json) => {
+    //     console.log(json);
+    //   });
+  }, []);
+
+  /* Mock Data for my sentitive tummy*/
+
   return (
     <View style={formatStyler.center}>
       <Image
@@ -12,7 +27,10 @@ function ProfileScreen0({ navigation }) {
           uri: "https://i.kym-cdn.com/photos/images/newsfeed/000/167/256/6rur.jpg?1314468464",
         }}
       />
-      <Text>Tester</Text>
+      <Text>Placeholder Joe</Text>
+      <Text>placedholder@amazon.com</Text>
+      <Text>Dietary Restrictions</Text>
+      {/* <FlatList /> */}
     </View>
   );
 }
